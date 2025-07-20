981b84d8ad1b52b5083267fa2ecf3eec64a342ac
Jose <josefbautista94@gmail.com>
2025-07-19 22:32:26 -0400

    uploading typescript solution

---
+++ typescript/Who likes it?.ts
@@ -0,0 +1,21 @@
+export const likes = (a : string[]) : string => {
+  // TODO
+  
+  if( a.length === 0){
+  return `no one likes this`
+    }
+  else if(a.length === 1){
+    return `${a[0]} likes this`
+  }
+  else if(a.length === 2){
+    return `${a[0]} and ${a[1]} like this`
+    } 
+    else if(a.length === 3){
+    return `${a[0]}, ${a[1]} and ${a[2]} like this`
+    } 
+  
+    else{
+    return `${a[0]}, ${a[1]} and ${a.length - 2} others like this`
+    } 
+  
+}
