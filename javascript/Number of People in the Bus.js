4e6f1684e6ac4e3762f03a55361f28d539b72342
Jose <josefbautista94@gmail.com>
2026-04-30 03:37:28 -0400

    Completed Number of People in the Bus in JS but this time with the reduce function

--- javascript/Number of People in the Bus.js
+++ javascript/Number of People in the Bus.js
@@ -1,12 +1,5 @@
 var number = function(busStops){
-  
-  let pplOn = 0;
-  let pplOff = 0;
-  
-  for(let i = 0; i < busStops.length; i++){
-    pplOn += busStops[i][0]
-    pplOff += busStops[i][1]
-  }
-  
-  return pplOn - pplOff;
+return busStops.reduce((people,[on,off]) =>{
+  return people + on - off;
+},0)
 }
