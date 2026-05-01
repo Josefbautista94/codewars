554a07e5049cf2c58ed5e54d78853d45498207a8
Jose <josefbautista94@gmail.com>
2026-05-01 04:34:16 -0400

    WeIrD StRiNg CaSe

---
+++ javascript/WeIrD StRiNg CaSe.js
@@ -0,0 +1,5 @@
+function toWeirdCase(string){
+  
+return string.split(' ').map(word => word.split('').map((char,i) => i % 2 === 0 ? char.toUpperCase() : char.toLowerCase()).join('')).join(' ');
+
+}
